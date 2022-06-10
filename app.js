1128ef7b26c2be872e8d80451f1a5ecbf5860354
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-10 22:49:44 +0200

    Add routes and send html chunks

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-let http = require("http");
+const http = require("http");
 
-let app = http
-  .createServer((req, res) => {
-    res.write("Hello, world!");
-    res.end();
-  })
-  .listen(8080);
+const app = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  if (req.url == "/") {
+    res.write("<html>");
+    res.write("<head><title>My Server</title></head>");
+    res.write("<body>");
+    res.write("<h1>My Server</h1>");
+    res.write("</body>");
+    res.write("<html>");
+  } else if (req.url == "/login") {
+    res.write("<html>");
+    res.write("<head><title>LOGIN</title></head>");
+    res.write("<body>");
+    res.write("<h1>THIS IS THE LOGIN PAGE</h1>");
+    res.write("</body>");
+    res.write("<html>");
+  }
+  res.end();
+});
+
+app.listen(8080);

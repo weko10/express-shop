c796705c80d428f1f56661971104562e29784b50
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-11 18:01:30 +0200

    Add html file

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const app = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
   if (req.url == "/") {
-    res.write("<html>");
-    res.write("<head><title>My Server</title></head>");
-    res.write("<body>");
-    res.write("<h1>My Server</h1>");
-    res.write("</body>");
-    res.write("<html>");
+    let file = fs.readFileSync("home.html");
+    res.write(file);
   } else if (req.url == "/login") {
     res.write("<html>");
     res.write("<head><title>LOGIN</title></head>");

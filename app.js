e2dcbcb85ae24faf462433df0387316ab88f2049
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-23 17:29:50 +0200

    Add comments

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// third-party packages
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// custom packages
 const shopRouter = require("./routes/shop");
 const adminRouter = require("./routes/admin");
 
 const app = express();
 
+// plugged in middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// custom middleware
 app.use(shopRouter);
 app.use(adminRouter);
 

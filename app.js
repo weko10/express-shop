e023bd2e2835708eab4051d01c3d7a8c7d861851
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-23 17:18:57 +0200

    Add body-parser and use it

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.send("<h1>My second express app</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Submit</button><form>'
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body.product);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("first handler");
+  res.send("<h1>Home</h1>");
 });
 
 app.listen(8080);

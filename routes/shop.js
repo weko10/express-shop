5506d91d63c2b550fbc3f38a8cc29b0251bcc9ab
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-25 02:08:46 +0200

    Add dynamic data to ejs file

--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
 const path = require("path");
+const fs = require("fs");
 
 router.get("/", (req, res, next) => {
-  res.render(path.join(__dirname, "..", "views", "shop.ejs"));
+  fs.readFile("./data.json", "utf-8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    const parsedData = JSON.parse(data);
+    res.render(path.join(__dirname, "..", "views", "shop.ejs"), {
+      productList: parsedData.productList,
+    });
+  });
 });
 
 module.exports = router;

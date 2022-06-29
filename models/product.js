e035126ee96b72ee799fe694ff83c131f7b7999f
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-29 14:32:41 +0200

    Create Product model

---
+++ models/product.js
@@ -0,0 +1,45 @@
+const fs = require("fs").promises;
+
+module.exports = class Product {
+  constructor(title, price, description, imgLink) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imgLink = imgLink;
+  }
+
+  static getAllProducts() {
+    return fs.readFile("./products.json", "utf-8").then(data => {
+      return JSON.parse(data).productList;
+    });
+  }
+
+  save() {
+    // update products file
+
+    fs.readFile("./products.json", "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const parsedData = JSON.parse(data);
+      const productList = parsedData.productList;
+
+      // push new product to list
+      productList.push(JSON.parse(JSON.stringify(newProduct)));
+
+      // rewrite data to file
+      fs.writeFile(
+        "./products.json",
+        JSON.stringify({ productList: productList }, null, 2),
+        err => {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log("Successfully added product:", newProduct);
+          }
+        }
+      );
+    });
+  }
+};

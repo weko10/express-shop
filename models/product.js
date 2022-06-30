9438e0f5c406e3ff578ae117e290b11883d840f5
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-30 13:32:30 +0200

    Use list of objs directly in products.json

--- models/product.js
+++ models/product.js
@@ -10,7 +10,8 @@ module.exports = class Product {
 
   static getAllProducts() {
     return fs.readFile("./products.json", "utf-8").then(data => {
-      return JSON.parse(data).productList;
+      // data => list of products
+      return JSON.parse(data);
     });
   }
 
@@ -19,38 +20,13 @@ module.exports = class Product {
 
     fs.readFile("./products.json", "utf-8")
       .then(data => {
-        const parsedData = JSON.parse(data);
-        const productList = parsedData.productList;
+        const productList = JSON.parse(data);
 
         // push new product to list
         productList.push(JSON.parse(JSON.stringify(this)));
 
-        fs.writeFile(
-          "./products.json",
-          JSON.stringify({ productList: productList }, null, 2)
-        );
+        fs.writeFile("./products.json", JSON.stringify(productList, null, 2));
       })
       .then(() => console.log("Success!"));
-
-    // (err, data) => {
-    //   const parsedData = JSON.parse(data);
-    //   const productList = parsedData.productList;
-
-    //   // push new product to list
-    //   productList.push(JSON.parse(JSON.stringify(this)));
-
-    //   // rewrite data to file
-    //   fs.writeFile(
-    //     "./products.json",
-    //     JSON.stringify({ productList: productList }, null, 2),
-    //     err => {
-    //       if (err) {
-    //         console.error(err);
-    //       } else {
-    //         console.log("Successfully added product:", newProduct);
-    //       }
-    //     }
-    //   );
-    // });
   }
 };

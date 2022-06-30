7a48aad3973a3aec29c29a1058848f8621248b7a
Waleyeldeen <waleyeldeenahmed@gmail.com>
2022-06-30 14:31:00 +0200

    Add more routes (without controllers)

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,20 @@
 const router = require("express").Router();
 const productsController = require("../controllers/products");
 
+// index page
 router.get("/", productsController.getHome);
+
+// shows list of products (e.g. redirected to after using search box)
+router.get("/products", productsController.getHome);
+
+// shows user cart
+router.get("cart", () => {
+  return;
+});
+
+// checkout page (e.g. payment, address)
+router.get("checkout", () => {
+  return;
+});
 
 module.exports = router;
